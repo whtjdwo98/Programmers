fd298072a97a5d10a33cb8bba770f5386f87a6a9
CHO SEONG JAE <37203984+whtjdwo98@users.noreply.github.com>
2024-05-15 01:30:31 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -0,0 +1,37 @@
+"""
+1. 하드디스크가 작업을 수행하고 있지 않을 때에는 먼저 요청이 들어온 작업부터 처리
+2. 작업이 끝났을 때 요청들어온 작업들이 여러 개라면, 요청 시간 + 작업 시간이 작은 작업부터 처리
+3. heapq는 첫 번째 요소를 기준으로 힙 구성
+4. 작업완료시간이 작은 순서부터 수행할 경우 평균 시간 짧아짐
+"""
+import heapq as h
+def solution(jobs):
+    answer = 0
+    time = 0
+    stays = []
+    h.heapify(jobs)
+    h.heapify(stays)
+    lenjobs = len(jobs)
+    while 1:
+        while 1:
+            try:
+                a = h.heappop(jobs)
+            except:
+                break
+            if a[0] <= time:
+                h.heappush(stays, [a[1], a[0]])
+            else:
+                h.heappush(jobs, a)
+                break
+        try:
+            completetime = h.heappop(stays)
+            
+            if time > completetime[1]:
+                answer += time + completetime[0]- completetime[1]
+            else:
+                answer += completetime[0]
+            time += completetime[0]
+        except:
+            if not jobs and not stays:
+                return answer // lenjobs
+            time += 1
