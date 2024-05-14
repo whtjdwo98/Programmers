d8329c03cc91e9b59bd7773287aa09ab3c9f107d
CHO SEONG JAE <37203984+whtjdwo98@users.noreply.github.com>
2024-05-15 01:56:36 +0900

    Update 디스크 컨트롤러.py

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -4,6 +4,7 @@
 3. 작업이 끝났을 때 요청들어온 작업들이 여러 개라면, 작업 시간이 작은 작업부터 처리
 4. 시간복잡도O(nlogn) : 힙 구성 = O(n), 최대 n번의(heappop O(logn) + heappush O(logn)) = O(nlogn)
 5. 헤맨 부분 : 문제의 소수점 나머지는 버리는 부분 놓쳐서 계속 틀림
+6. 아쉬운 점 : 맨 마지막 time += 1 -> 여기서 불필요한 반복문이 계속됨 -> 작업 대기중인heap의 가장 작은 요청시간을 time에 더해버리면 불필요한 반복문 줄어들 것으로 예상
 """
 import heapq as h
 def solution(jobs):
