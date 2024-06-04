72056db161ae329579f5671ca47b89923d9af08f
CHO SEONG JAE <37203984+whtjdwo98@users.noreply.github.com>
2024-06-04 21:40:15 +0900

    [level 3] Title: 가장 먼 노드, Time: 101.65 ms, Memory: 21.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -0,0 +1,41 @@
+distance_list = []
+graph = {}
+def solution(n, edge):
+    init(n, edge)
+    return find_distance()
+
+def init(n, edge):
+    global distance_list
+    global graph
+    distance_list = [1] * (n + 1)  #거리 리스트 노드 수 만큼 초기화
+    distance_list[0] = 1
+    for i in edge:  #주어진 edge를 python dictionary로 선언
+        if i[0] in graph:  #양방향 간선이기 때문에 조건문 2개 사용하여 dictionary에 정보 삽입
+            temp = graph[i[0]]
+            temp.append(i[1])
+            graph[i[0]] = temp
+        else:
+            graph[i[0]] = [i[1]]
+        if i[1] in graph:
+            temp = graph[i[1]]
+            temp.append(i[0])
+            graph[i[1]] = temp
+        else:
+            graph[i[1]] = [i[0]]
+
+def find_distance():
+    global distance_list
+    global graph
+    queue = [1]
+    distance = 1
+    while len(queue) > 0: #노드1 에서 각노드까지의 최단 거리 계산
+        current = queue.pop(0)
+        
+        temp = graph[current]
+        distance = distance_list[current] + 1
+        for i in temp:
+            if (distance_list[i] == 1 or distance_list[i] > distance) and i != 1:
+                queue.append(i)
+                distance_list[i] = distance
+    maximum = max(distance_list) #최댄거리 계산 후 최대값의 갯수 리턴
+    return distance_list.count(maximum)
